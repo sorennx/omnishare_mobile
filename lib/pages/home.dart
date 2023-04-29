178830b78fa6abedea15055acdfc3072bbcc6746
sorennx <sorennx@gmail.com>
2023-04-29 21:54:02 +0200

    Added pages & safe area

---
+++ lib/pages/home.dart
@@ -0,0 +1,19 @@
+import '../themes/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text("Home"),
+    ));
+  }
+}
